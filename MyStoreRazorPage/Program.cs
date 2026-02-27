4461ab0caf938956b05bb5af5c9b7933636d4f0c
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2026-02-27 21:46:49 +0700

    add: program.cs

--- MyStoreRazorPage/Program.cs
+++ MyStoreRazorPage/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MyStore.Business.LocNT;
+using MyStore.Repositories.LocNT;
+using MyStore.Services.LocNT;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,11 +15,17 @@ builder.Services.AddDbContext<MyStoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaulConnection")));
 
 //repository
-
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 //service
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 
+//build
 var app = builder.Build();
 
 
